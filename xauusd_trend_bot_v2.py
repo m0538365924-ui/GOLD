8bef62f856be604df6a7775519e2ee925e4ad854
m0538365924-ui <m0538365924@gmail.com>
2026-03-23 11:08:11 +0300

    Update xauusd_trend_bot_v2.py
    تغيير عدد الشموع

--- xauusd_trend_bot_v2.py
+++ xauusd_trend_bot_v2.py
@@ -57,7 +57,7 @@ PAIRS = {
 
 STRATEGY_TF   = os.getenv('STRATEGY_TF', 'MINUTE_15')
 CANDLES_COUNT = 500
-SCAN_INTERVAL = int(os.getenv('SCAN_INTERVAL', '300'))
+SCAN_INTERVAL = int(os.getenv('SCAN_INTERVAL', '30000'))
 
 LENGTH       = int(os.getenv('LENGTH',      '10'))
 SLOPE_MULT   = float(os.getenv('SLOPE_MULT', '1.0'))
